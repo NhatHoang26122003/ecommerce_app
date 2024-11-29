3e39e5bd24e9f2b3c182c4114cbf97939fb0e6f4
NhatHoang26122003 <nhathoang261203@gmail.com>
2024-11-29 10:12:09 +0700

    update_get_started

--- lib/screen/auth/get_started.dart
+++ lib/screen/auth/get_started.dart
@@ -16,58 +16,72 @@ class GetStartedScreen extends StatelessWidget {
           Positioned(
             left: 0,
             right: 0,
-            top: MediaQuery.of(context).size.height * 0.6,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  child: Text(
-                    "You want Authentic, here you go!",
+            top: MediaQuery.of(context).size.height * 0.5,
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: Container(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Colors.black.withOpacity(1),
+                    Colors.black.withOpacity(0.0),
+                  ]
+                )
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    child: Text(
+                      "You want Authentic, here you go!",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    padding: EdgeInsets.only(left: 20, right: 20),
+                  ),
+                  SizedBox(height: 5,),
+                  Text (
+                    "Find it here, buy it now!",
                     style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 35,
+                      fontSize: 15,
                       color: Colors.white,
                     ),
-                    textAlign: TextAlign.center,
                   ),
-                  padding: EdgeInsets.only(left: 20, right: 20),
-                ),
-                SizedBox(height: 5,),
-                Text (
-                  "Find it here, buy it now!",
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 20,),
-                Container(
-                  width: double.infinity,
-                  padding: EdgeInsets.symmetric( horizontal: 60),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(context,
-                      MaterialPageRoute(builder: (context) => HomePageScreen()));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: AppColor.titleButtonColor,
-                      padding: EdgeInsets.symmetric(vertical: 15),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      )
-                    ),
-                    child: Text (
-                      "Get Started",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+                  SizedBox(height: 20,),
+                  Container(
+                    width: double.infinity,
+                    padding: EdgeInsets.symmetric( horizontal: 60),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => HomePageScreen()));
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: AppColor.titleButtonColor,
+                        padding: EdgeInsets.symmetric(vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        )
+                      ),
+                      child: Text (
+                        "Get Started",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
                   ),
-                )
-              ],
+                  SizedBox(height: 20,)
+                ],
+              )
             )
           )
         ],
